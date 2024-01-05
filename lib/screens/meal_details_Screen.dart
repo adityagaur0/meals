2550e26134d70aa57067fae5e4511bf6f9812574
adityagaur0 <adityakumargaur0@gmail.com>
2024-01-05 01:44:33 +0000

    5 Jan 2024, 07:14

--- lib/screens/meal_details_Screen.dart
+++ lib/screens/meal_details_Screen.dart
@@ -63,11 +63,14 @@ class MealsDetailsScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(
               height: 14,
